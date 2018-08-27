f81fe59ee300258b2c47fc8f3061cdfbb3c4a9a9
dashkodo <dashkodo@gmail.com>
2018-08-27 17:14:52 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,5 @@
 <?php
-  $data = array( 'text' => file_get_contents("php://input"); );
+  $data = array( 'text' => file_get_contents("php://input") );
   $data_string= json_encode($data);
 
   $ch = curl_init('https://hooks.slack.com/services/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');
