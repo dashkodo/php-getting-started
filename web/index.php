1ead4e73a78f0b2c1435654cede801b54d4905f2
dashkodo <dashkodo@gmail.com>
2018-08-27 17:11:01 +0300

    Update index.php

--- web/index.php
+++ web/index.php
@@ -1,5 +1,6 @@
 <?php
-  $data_string= json_encode(array( 'text' => print_r($_REQUEST, true)));
+  $data = array( 'text' => $_REQUEST );
+  $data_string= json_encode($data);
 
   $ch = curl_init('https://hooks.slack.com/services/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');
   curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
